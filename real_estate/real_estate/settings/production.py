2aea38cdecae88f96810889507bf083dafbb56a4
prakhar-revemax <prakar.t@revemax.ai>
2024-08-07 18:17:37 +0530

    production and develpment environment setup

---
+++ real_estate/real_estate/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DATABASE = {
+    'default': {
+        'ENGIN': env('POSTGRES_ENGINE'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('PG_HOST'),
+        'PORT': env('PG_PORT'),
+    }
+}
+
